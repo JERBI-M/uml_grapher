5b4c07a78db8f2ad90cf6a07d5c0194372eaaf70
Morwane <jerbi.morwane@gmail.com>
2023-01-15 11:23:23 +0100

    Test UmlGraphTests Part 1

--- src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
+++ src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
@@ -1,30 +1,24 @@
 package fr.lernejo.umlgrapher;
-import java.lang.reflect.Modifier;
 
 public class UmlGraph {
-    
-	private final Class [] allClasses;
-        public UmlGraph(Class[] allClasses) {
-        this.allClasses = allClasses;
-    }
 
+	public UmlGraph (Class class_name){
 
-    public String as(GraphType graphType) {
-	String out ="";
-	if(graphType == GraphType.Mermaid) 
-	{ 
-	try{   
-           InternalGraphRepresentation graphiq = new InternalGraphRepresentation(allClasses);
-           out = new MermaidFormatter().shape(graphiq);
-	}catch (RuntimeException e){
-           System.out.println("There is an error :" +e.getClass() + "-" + e.getMessage()); 
 	}
 
-              
-        }
-	return out;
-        
-        }
+	public String as(GraphType graphType){
+
+		return """
+		classDiagram
+		class Machin {
+			<<interface>>
+		}
+
+		""";
+
+	}
+    
+	
 
     }
 
